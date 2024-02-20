a98781b50ce437005a6dcf1e26af5f3a48c1991a
haaker1 <haaker.anne@gmail.com>
2024-02-20 13:52:50 +0100

    refactor: rename test

--- src/test/java/org/mockito/internal/matchers/EqualsTest.java
+++ src/test/java/org/mockito/internal/matchers/EqualsTest.java
@@ -29,9 +29,10 @@ public class EqualsTest extends TestBase {
     }
 
     @Test
-    public void shouldArraysBeNotEqual(){
+    public void assertFalseWhenArraysNotEqual(){
         assertFalse(new ArrayEquals(new int[] {1, 2}).matches(new boolean[] {true, false}));
     }
+    
 
     @Test
     public void shouldDescribeWithExtraTypeInfo() throws Exception {
